31700ff2c09a95dbfe6ca6ec7057a832a67b167c
Tze Wei <tzeweiwee@gmail.com>
2018-08-12 05:26:24 +0800

    setup mock db and db client

---
+++ login-app/db/db.js
@@ -0,0 +1,14 @@
+export default {
+  users: [
+    {
+      id: 1,
+      email: 'ali@hotmail.com',
+      password: 1234
+    },
+    {
+      id: 2,
+      email: 'muthu@yahoo.com',
+      password: 1234
+    }
+  ]
+}
---
+++ login-app/lib/mock/dbHelper.js
@@ -0,0 +1,37 @@
+class DBHelper {
+  constructor(collectionName) {
+    this.collectionName = collectionName;
+  }
+
+  connect() {
+    this.collection = require('../../db/db').default[this.collectionName];
+    if(!this.collection) {
+      throw new Error('collection not found');
+    }
+  }
+
+  closeConnection() {
+    return true;
+  }
+
+  findOne(query) {
+    return this.collection.find(data => {
+      let found = false;
+      for(let prop in query) {
+        if(data.hasOwnProperty(prop)) {
+          console.log(data[prop], query[prop]);
+          if(data[prop] != query[prop]) {
+            found = false;
+            break;
+          }
+          found = true;
+        }
+      }
+      if(found) {
+        return true;
+      }
+    });
+  }
+}
+
+export default DBHelper;
