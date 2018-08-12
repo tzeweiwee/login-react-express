f9e9a8edc28f790bea3860a95da8839c5695f488
Tze Wei <tzeweiwee@gmail.com>
2018-08-12 15:14:39 +0800

    add validation for POSTLOGIN

--- login-app/lib/POSTLOGIN.js
+++ login-app/lib/POSTLOGIN.js
@@ -1,24 +1,39 @@
-import DBClient from './mock/dbHelper';
+import DBClient from "./mock/dbHelper";
 
 const collectionName = "users";
 
-function userLogin (user) {
+function validate({ email, password }) {
+  if (email && password) {
+    if (typeof email !== "string") {
+      throw new Error("Invalid email.");
+    }
+
+    if (typeof password !== "string" || typeof password !== "number") {
+      throw new Error("Invalid password.");
+    }
+  }
+  throw new Error("Invalid email or password.");
+}
+
+function userLogin(user) {
   const client = new DBClient(collectionName);
   client.connect();
-  if(client.findOne(user)) {
+  if (client.findOne(user)) {
     client.closeConnection();
     return;
   } else {
-    throw new Error('Invalid email or password. User not found.')
+    throw new Error("Invalid email or password. User not found.");
   }
 }
 
-export default function (req, res) {
+export default function(req, res) {
   try {
-    userLogin(req.body);
-    res.status(200).send({body: 'Login successful'});
+    const data = req.body;
+    validate(data);
+    userLogin(data);
+    res.status(200).send({ body: "Login successful" });
   } catch (err) {
     res.statusMessage = err.message;
-    res.status(500).send({body: 'Invalid email or password'});
+    res.status(500).send({ body: "Invalid email or password" });
   }
 }
